4bdc8f0f65a9c8a97f2cf2f1b8f40cfbb2f4a092
PratisthaMaharjan <pratistha.maharjan@hotmail.com>
2024-10-18 22:12:54 +0545

    Day 2: task-III complete solution.

--- Day2/Task-III/script.js
+++ Day2/Task-III/script.js
@@ -4,3 +4,9 @@ for (let x of animal) {
     let result = x.includes("a");
     console.log(`${x}: ${result}`);
 }
+
+const newAnimal = animal.filter((name) => {
+    return !name.includes("a");
+});
+
+console.log(newAnimal);
